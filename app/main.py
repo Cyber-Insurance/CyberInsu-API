742cdbf7403d8abcb5403e984cbb9decd8721d3a
AdamAh0 <adam.ahmani11@gmail.com>
2026-06-10 14:14:44 +0200

    fix le main

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ app = FastAPI(title=settings.APP_NAME, version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://cyberinsu-front-end-production.up.railway.app",  # ← ajoute cette ligne
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
